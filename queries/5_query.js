5a02b84c7ad2e536e0b48171059809791b922ae6
estebanmontoyaaranda20 <44998533+estebanmontoyaaranda20@users.noreply.github.com>
2022-04-02 21:31:07 +0200

    Update 5_query.js

--- queries/5_query.js
+++ queries/5_query.js
@@ -1,1 +1,2 @@
+//5. Pacients amb més de dos nòduls. Mostra ID del Pacient, sexe, edat, diagnòstic del Pacient
 db.Patients.find({Nodules: {$gt: 2}}).projection({PatientID:1,Age:1,Gender:1,DiagnosisPatient:1})
