1c8f54541343524269455fcc59658fc00907eec6
estebanmontoyaaranda20 <44998533+estebanmontoyaaranda20@users.noreply.github.com>
2022-04-02 21:31:36 +0200

    Update 6_query.js

--- queries/6_query.js
+++ queries/6_query.js
@@ -1,1 +1,2 @@
+//6. Mostrar els 4 mètodes amb més repeticions de l’experiment. Mostra el ID del Mètode i número de repeticions de l’experiment.
 db.Experiments.find().limit(4).sort( { Repetition: -1 }).projection({MethodID:1,Repetition:1})
